6e756c618566d8f37462092d545e6293ce9e1e1e
RabbitFored <archeopterxyfoss@gmail.com>
2026-04-11 10:04:00 +0000

    feat: add Linux build support to forge config and GitHub Actions workflow

--- forge.config.js
+++ forge.config.js
@@ -8,63 +8,49 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-        "name": "@electron-forge/maker-squirrel",
-       "config": {
-          "name": "BLAYe",
-          "authors": "TheOstrich",
-          "exe": "BLAYe.exe",
-          "setupExe": "BLAYe-Setup.exe",
-          //"setupIcon": "./assets/icon.ico",
-          //"iconUrl": "https://raw.githubusercontent.com/RabbitFored/BLAYe/main/assets/icon.ico",
-          //"loadingGif": "./assets/installing.gif",
-          "createDesktopShortcut": true,
-          "createStartMenuShortcut": true
-       },
-        "platforms": ["win32"]
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "BLAYe",
+        authors: "TheOstrich",
+        exe: "BLAYe.exe",
+        setupExe: "BLAYe-Setup.exe",
+        createDesktopShortcut: true,
+        createStartMenuShortcut: true
       },
-      {
-    "name": "@electron-forge/maker-wix",
-    "config": {
-      "name": "BLAYe Professional Billing",
-      "manufacturer": "THEOSTRICH",
-      "ui": { "chooseDirectory": true }
+      platforms: ["win32"]
     },
-    "platforms": ["win32"]
-  },
-
-      {
-        "name": "@electron-forge/maker-zip",
-        "platforms": ["darwin", "linux", "win32"]
+    {
+      name: "@electron-forge/maker-wix",
+      config: {
+        name: "BLAYe Professional Billing",
+        manufacturer: "THEOSTRICH",
+        ui: { "chooseDirectory": true }
+      },
+      platforms: ["win32"]
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "linux", "win32"]
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          maintainer: "RabbitFoRed",
+          homepage: "https://github.com/RabbitFored/BLAYe"
+        }
       }
-   // {
-   //   name: '@electron-forge/maker-wix', // NEW: This makes the .msi
-   //   config: {
-   //     ui: {
-   //       chooseDirectory: true
-   //     },
-   //     manufacturer: 'THE OSTRICH inc.' // Add your name here
-   //   }
-   // },
-    //{
-    // name: '@electron-forge/maker-zip',
-    // platforms: ['darwin'],
-   // },
-    //{
-     // name: '@electron-forge/maker-deb',
-     // config: {},
-    //},
-    //{
-    //  name: '@electron-forge/maker-rpm',
-     // config: {},
-//},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {}
+    }
   ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
